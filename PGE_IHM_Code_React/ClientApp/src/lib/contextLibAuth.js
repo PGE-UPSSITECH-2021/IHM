342cfaafda5b6ad5ac5566c02505321aec3e78e6
JulieP-B <julie.pivin-bachler@univ-tlse3.fr>
2021-11-27 15:53:10 +0100

    Clean evertyhing
    Besoin de tout supprimer car bug sur VS

--- PGE_IHM_Code_React/ClientApp/src/lib/contextLibAuth.js
+++
@@ -1,7 +0,0 @@
-﻿import { useContext, createContext } from "react";
-
-export const AppContextAuth = createContext(null);
-
-export function useAppContextAuth() {
-    return useContext(AppContextAuth);
-}
--- PGE_IHM_Code_React/ClientApp/src/lib/contextLibWrongID.js
+++
@@ -1,7 +0,0 @@
-﻿import { useContext, createContext } from "react";
-
-export const AppContextWrongID = createContext(null);
-
-export function useAppContextWrongID() {
-    return useContext(AppContextWrongID);
-}
