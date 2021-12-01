38003f17a7407ef88906fecb5dc279560202ccb5
JulieP-B <julie.pivin-bachler@univ-tlse3.fr>
2021-12-01 20:39:13 +0100

    Intégration nouveau logo et nom du PGE

--- ReactIHM_PGE/ReactIHM_PGE/ClientApp/src/components/TopBanner.js
+++ ReactIHM_PGE/ReactIHM_PGE/ClientApp/src/components/TopBanner.js
@@ -1,11 +1,11 @@
 import '../styles/TopBanner.css'
 import logo from '../assets/logo.png'
-import logo_upssi from '../assets/logo_upssi.png'
+import logo_upssi from '../assets/logoDBRIF.png'
 
 
 function TopBanner() {
 	return (<div className='pge-topbanner'>
-        <h1 className='pge-title'>Système de Vision</h1>
+        <h1 className='pge-title'>DBRIF</h1>
         <img src={logo} alt='logo eXcent' className='pge-logo-eXcent' />
         <img src={logo_upssi} alt='logo upssitech' className='pge-logo-upssi' />
     </div>)
