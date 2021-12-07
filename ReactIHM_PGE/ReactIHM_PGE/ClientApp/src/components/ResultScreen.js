472a73d9e94f6a69fc068f0be6198582b0a71051
Enzo2532 <93335225+Enzo2532@users.noreply.github.com>
2021-12-07 11:52:32 +0100

    Add files via upload

--- ReactIHM_PGE/ReactIHM_PGE/ClientApp/src/components/ResultScreen.js
+++ ReactIHM_PGE/ReactIHM_PGE/ClientApp/src/components/ResultScreen.js
@@ -1,14 +1,13 @@
 ﻿import '../styles/ResultScreen.css'
 import MenuBar from './MenuBar'
 import MiddleResultScreen from './MiddleResultScreen'
-import ResultIndications from './ResultIndications'
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function ResultScreen() {
     return (
         <div className='mainResult'>
-            <span id='menu-bar'><MenuBar /></span>
+            <span className='menu-bar'><MenuBar /></span>
             {/*<span className='middle-result-screen'><MiddleResultScreen /></span>*/}
             <MiddleResultScreen />
 
