4c7e5177ad75a8a8d91be182792ac94a442bf3d7
JulieP-B <julie.pivin-bachler@univ-tlse3.fr>
2022-01-19 08:35:39 +0100

    ParamUsr mdp et mail OK

--- ReactIHM_PGE/ReactIHM_PGE/ClientApp/src/components/PasswordStrength.js
+++ ReactIHM_PGE/ReactIHM_PGE/ClientApp/src/components/PasswordStrength.js
@@ -2,21 +2,24 @@
 import '../styles/PasswordStrength.css'
 import CheckListPwd from './CheckListPwd';
 import Button from 'react-bootstrap/Button';
-
+import { BiShowAlt } from "react-icons/bi";
+import { BiHide } from "react-icons/bi";
 
 function PasswordStrength() {
 
     const [newPassword, setNewPassword] = useState("");
     const [passwordConfirm, setPasswordConfirm] = useState("");
 
-    const [containsUL, setContainsUL] = useState(false)
-    const [containsLL, setContainsLL] = useState(false)
-    const [containsN, setContainsN] = useState(false)
-    const [containsSC, setContainsSC] = useState(false)
-    const [contains7C, setContains7C] = useState(false)
-    const [passwordMatch, setPasswordMatch] = useState(false)
+    const [containsUL, setContainsUL] = useState(false);
+    const [containsLL, setContainsLL] = useState(false);
+    const [containsN, setContainsN] = useState(false);
+    const [containsSC, setContainsSC] = useState(false);
+    const [contains7C, setContains7C] = useState(false);
+    const [passwordMatch, setPasswordMatch] = useState(false);
 
-    const [checkListValid, setCheckListValid] = useState(false)
+    const [checkListValid, setCheckListValid] = useState(false);
+
+    const [showMust, setShowMust] = useState(false);
 
     const checkListData = [
         ["Une lettre minuscule (a-z)", containsLL],
@@ -27,50 +30,103 @@ function PasswordStrength() {
         ["Mot de passe match", passwordMatch]
     ]
 
+    const [passwordIsVisible, setPasswordIsVisible] = useState(false);
+    const [passwordConfirmIsVisible, setPasswordConfirmIsVisible] = useState(false);
+
     const validatePassword = () => {
 
-        if (newPassword.toLowerCase() !== newPassword) setContainsUL(true)
-        else setContainsUL(false)
+        if (newPassword.toLowerCase() !== newPassword) {
+            setContainsUL(true);
+        } else {
+            setContainsUL(false);
+        }
+        if (newPassword.toUpperCase() !== newPassword) {
+            setContainsLL(true);
+        } else {
+            setContainsLL(false);
+        }
+        if (/\d/.test(newPassword)) {
+            setContainsN(true);
+        } else {
+            setContainsN(false);
+        }
+        if (/[~`!@#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(newPassword)) {
+            setContainsSC(true);
+        } else {
+            setContainsSC(false);
+        }
+        if (newPassword.length >= 7) {
+            setContains7C(true);
+        } else {
+            setContains7C(false);
+        }
+        if (newPassword !== "" && newPassword === passwordConfirm) {
+            setPasswordMatch(true);
+        } else {
+            setPasswordMatch(false);
+        }
+        if (containsUL && containsLL && containsN && containsSC && contains7C && passwordMatch) {
+            setCheckListValid(true);
+        } else {
+            setCheckListValid(false);
+        }
+    }
 
-        if (newPassword.toUpperCase() !== newPassword) setContainsLL(true)
-        else setContainsLL(false)
 
-        if (/\d/.test(newPassword)) setContainsN(true)
-        else setContainsN(false)
+    function enableSave() {
+        return checkListValid;
+    }
 
-        if (/[~`!@#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(newPassword)) setContainsSC(true)
-        else setContainsSC(false)
+    function passwordModif(event) {
+        setShowMust(true);
+        setNewPassword(event.target.value);
 
-        if (newPassword.length >= 7) setContains7C(true)
-        else setContains7C(false)
+    }
+    function confirmPasswordModif(event) {
+        setPasswordConfirm(event.target.value);
+    }
 
-        if (newPassword !== "" && newPassword === passwordConfirm) setPasswordMatch(true)
-        else setPasswordMatch(false)
-
-        if (containsUL && containsLL && containsN && containsSC && contains7C && passwordMatch) setCheckListValid(true)
-        else setCheckListValid(false)
+    function saveNewPassword() {
+        setShowMust(false);
+        setNewPassword("");
+        setPasswordConfirm("");
+        setContainsUL(false);
+        alert("Mot de passe modifié !");
     }
 
     return (
         <div className="mdp-area">
             <span className='label-identifiant'> Nouveau mot de passe : </span>
-            <input type="password"
-                value={newPassword}
-                onChange={e => setNewPassword(e.target.value)}
-                onKeyUp={validatePassword} />
+            <div className="mdp-area-2">
+                <input
+                    className='pwd-new-confirm'
+                    type={passwordIsVisible ? 'text' : 'password'}
+                    value={newPassword}
+                    onChange={e => passwordModif(e)}
+                    onKeyUp={validatePassword}
+                    />
+                <button onClick={() => setPasswordIsVisible(!passwordIsVisible)}> {passwordIsVisible ? <BiShowAlt /> : <BiHide />} </button>
+            </div>
             <span className='label-identifiant'> Confirmation mot de passe : </span>
-            <input type="password"
-                value={passwordConfirm}
-                onChange={e => setPasswordConfirm(e.target.value)}
-                onKeyUp={validatePassword} />
-
-            <Button block size="lg" type="submit" disabled={!checkListValid} className='mdp-sauv-button'>
+            <div className="mdp-area-2">
+                <input
+                    className='pwd-new-confirm'
+                    type={passwordConfirmIsVisible ? 'text' : 'password'}
+                    value={passwordConfirm}
+                    onChange={e => confirmPasswordModif(e)}
+                    onKeyUp={validatePassword}
+                    />
+                <button onClick={() => setPasswordConfirmIsVisible(!passwordConfirmIsVisible)}> {passwordConfirmIsVisible ? <BiShowAlt /> : <BiHide />} </button>
+            </div>
+            <Button block size="lg" type="submit" disabled={!(containsUL && containsLL && containsN && containsSC && contains7C && passwordMatch)} className='mdp-sauv-button' onClick={saveNewPassword}>
                 Enregistrer
             </Button>
             <br></br>
-            <div className="must-container">
-                {checkListData.map(data => <CheckListPwd data={data} />)}
-            </div>
+            {showMust ?
+                <div className="must-container">
+                    {checkListData.map(data => <CheckListPwd data={data} />)}
+                </div> : <div className="must-container-invisible"> </div>
+            }
         </div>
 
     );
